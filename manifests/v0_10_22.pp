4a1c3b008e6017f13e5013cfe9091fe55f4e6f58
Greg Thornton <xdissent@me.com>
2013-11-24 12:04:14 -0600

    Add node 0.10.22

---
+++ manifests/v0_10_22.pp
@@ -0,0 +1,9 @@
+# Public: Install nodejs v0.10.22
+#
+# Usage:
+#
+#   include nodejs::v0_10_22
+
+class nodejs::v0_10_22 {
+  nodejs::version { 'v0.10.22': }
+}
--- spec/classes/nodejs__v0_10_spec.rb
+++ spec/classes/nodejs__v0_10_spec.rb
@@ -4,11 +4,11 @@ describe "nodejs::v0_10" do
   let(:facts) { default_test_facts }
 
   it do
-    should include_class("nodejs::v0_10_21")
+    should include_class("nodejs::v0_10_22")
 
     should contain_file("/test/boxen/nodenv/versions/v0.10").with({
       :ensure => "link",
-      :target => "/test/boxen/nodenv/versions/v0.10.21"
+      :target => "/test/boxen/nodenv/versions/v0.10.22"
     })
   end
 end
